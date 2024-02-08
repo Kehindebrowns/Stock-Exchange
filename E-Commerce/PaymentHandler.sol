39e4c71708bd61115f84fc69211654e8b1a56bd0
Kehindebrowns <111283128+Kehindebrowns@users.noreply.github.com>
2024-02-08 17:17:54 +0100

    Update PaymentHandler.sol

--- E-Commerce/PaymentHandler.sol
+++ E-Commerce/PaymentHandler.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.19;
 
 contract PaymentHandler {
  address public payable owner;    
 event PaymentReceived(address indexed _from, uint256 amount);
 
     function receivePayment() external payable {
-        require(msg.value > 0, "Payment must be greater than 0");
+        require(msg.value >= 0, "Payment must be greater than 0");
         
 emit PaymentReceived(msg.sender, msg.value);
     }
 
     function withdraw() external {
-        uint256 amountToWithdraw = 1 ether; // Specify the amount to withdraw
+        uint256 amountToWithdraw = 1 ether; 
         require(address(this).balance >= amountToWithdraw, "Insufficient balance");
        
         (bool success,) = msg.sender.call{value: amountToWithdraw}("");
