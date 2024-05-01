819f8647b92ba851d636930bddcd97290d206a13
Kehindebrowns <111283128+Kehindebrowns@users.noreply.github.com>
2024-05-01 05:15:36 -0600

    Update PaymentHandler.sol
    change the amout to a new one

--- E-Commerce/PaymentHandler.sol
+++ E-Commerce/PaymentHandler.sol
@@ -3,10 +3,11 @@ pragma solidity ^0.8.19;
 
 contract PaymentHandler {
  address public payable owner;   
-event PaymentReceived(address indexed _from, uint256 amount);
+event PaymentReceived(address indexed _from, uint256 _amount);
 
     function receivePayment() external payable {
         require(msg.value >= 0, "Payment must be greater than 0");
+       amount = _amount;
         
 emit PaymentReceived(msg.sender, msg.value);
     }
